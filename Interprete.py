e105be8a42cb4c32fc997c67800fbd51a569be28
V1centeJ1menez <vjimenez@usm.cl>
2024-08-14 02:18:11 -0400

    Parser v0.1

--- Interprete.py
+++ Interprete.py
@@ -1,6 +1,7 @@
 from tokenizer import Tokenizer
+from parser import Parser
 
-if __name__ == "__main__":
+def main():
 
     # Leer el archivo codigo.txt
     with open('codigo.txt', 'r') as f:
@@ -9,3 +10,17 @@ if __name__ == "__main__":
     # Tokenización
     tokenizer = Tokenizer(codigo_fuente)
     tokens = tokenizer.tokenizar()
+    contador = 0
+
+    for token in tokens:
+        contador += 1
+        print(f"Token nº{contador}: Tipo='{token.tipo}', Valor='{token.valor}', Línea={token.linea}")
+
+
+    parser = Parser(tokens)
+    parser.parse()
+    print("El código es sintácticamente correcto.")
+   
+
+if __name__ == "__main__":
+    main()
--- tokenizer.py
+++ tokenizer.py
@@ -19,13 +19,17 @@ patrones = [
         (r'(?P<par_cer>\))', 'PAR_CER'),
         (r'(?P<llave_abr>\{)', 'LLAVE_ABR'),
         (r'(?P<llave_cer>\})', 'LLAVE_CER'),
+        (r'(?P<nueva_linea>\n)', 'NUEVA_LINEA'),
         (r'(?P<espacio>\s+)', 'ESPACIO'),
+        (r'(?P<mismatch>.)', 'MISMATCH'),
     ]
 
 class Token:
-    def __init__(self, tipo, valor):
+    def __init__(self, tipo, valor, linea):
         self.tipo = tipo
         self.valor = valor
+        self.linea = linea
+        
 
 # Tokenizer
 class Tokenizer:
@@ -36,6 +40,7 @@ class Tokenizer:
         self.patrones_compilados = [(re.compile(patron), tipo) for patron, tipo in patrones]
 
     def tokenizar(self):
+        numero_linea = 1 # Inicializar el contador de líneas
         for linea in self.codigo_fuente:
             pos = 0
             while pos < len(linea):
@@ -43,16 +48,23 @@ class Tokenizer:
                 for patron, tipo in self.patrones_compilados:
                     match = patron.match(linea, pos)
                     if match:
-                        if tipo != 'ESPACIO':  # Ignorar espacios
+                        if tipo == 'ESPACIO':
+                            # Ignorar espacios
+                            pos = match.end(0)
+                            break
+                        elif tipo == 'MISMATCH':
+                            # Lanzar excepción para caracteres desconocidos
+                            caracter_no_conocido = match.group(0)
+                            raise Exception(f"Error: Caracter desconocido '{caracter_no_conocido}' en línea {numero_linea}")
+                        else:
                             valor = match.group(0)
-                            token = Token(tipo, valor)
+                            token = Token(tipo, valor, numero_linea)
                             self.tokens.append(token)
-                            self.imprimir_token(token)  # Imprimir el token
-                        pos = match.end(0)
-                        break
-                if not match:
-                    raise Exception(f"Error de tokenización en la línea: {linea}")
+                            # self.imprimir_token(token)  # Imprimir el token
+                            pos = match.end(0)
+                            break
+            numero_linea += 1  # Incrementar el contador de líneas
         return self.tokens
     
-    def imprimir_token(self, token):
-        print(f"Token: Tipo='{token.tipo}', Valor='{token.valor}'")
+    '''def imprimir_token(self, token):
+        print(f"Token: Tipo='{token.tipo}', Valor='{token.valor}', Línea={token.linea}")'''
